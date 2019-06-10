0d71f7a1001f31968ef6757ba1e7efffe2f0c3c8
Jim Fawkes <meilfort@protonmail.com>
2019-06-10 11:04:19 +0200

    Add basic cli functionality

---
+++ tweetpipe/cli.py
@@ -0,0 +1,55 @@
+"""CLI to run base tweetpipe
+
+"""
+
+import argparse
+import sys
+from loguru import logger
+
+# from config import Config
+
+_log_file_name = __file__.split("/")[-1].split(".")[0]
+logger.add(f"logs/tweetpipe_{_log_file_name}.log", rotation="1 day")
+
+# config = Config()
+
+help_text = """
+Project TweetPipe - A Contentful Challenge
+
+"""
+
+epilog = """
+
+Written as first draft by Moritz Eilfort.
+
+"""
+
+parser = argparse.ArgumentParser(
+    prog="tweetpipe", formatter_class=argparse.RawDescriptionHelpFormatter, description=help_text, epilog=epilog
+)
+
+parser.add_argument("--user_handle", "-u", help="Twitter User Handle (@'handle')", type=str, required=True)
+
+parser.add_argument("--count", "-c", help="Nuber of recent tweets to retrieve", type=int, default=5)
+
+# Do not upload fetched tweets to s3
+# parser.add_argument("--avoid_s3", action="store_true", help="Do not upload to S3")
+
+# Should the transformation phase load old tweets from S3 instead of fetching new ones
+# parser.add_argument("--read_from_s3", help="Read data from local file, clean, validate and store the data.", type=str)
+
+
+def run_pipeline(userhandle, count):
+    logger.warning(f"NOT YET IMPLEMENTED")
+    logger.debug(f"Fetch last {count} tweets for '{userhandle}'")
+
+
+def main():
+    args = parser.parse_args()
+    logger.debug(f"Starting TweetPipe")
+
+    run_pipeline(str(args.user_handle), int(args.count))
+
+
+if __name__ == "__main__":
+    main()
