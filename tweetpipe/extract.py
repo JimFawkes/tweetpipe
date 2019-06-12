acb6ad3f84cd870f0a8c33940238eba257ad8a8c
Jim Fawkes <meilfort@protonmail.com>
2019-06-12 23:36:59 +0200

    Refactor to use storage.S3 to interact with S3

--- tweetpipe/extract.py
+++ tweetpipe/extract.py
@@ -1,3 +1,11 @@
+"""
+Extract tweets for a given user.
+
+Use tweepy to interact with the twitter api.
+
+After fetching the data, also upload the raw data along with metadata to S3,
+incase the later steps need to be rerun at some point.
+"""
 import boto3
 import tweepy
 import json
@@ -6,10 +14,10 @@ from loguru import logger
 from django.utils import timezone
 
 from config import settings
+from storage import S3
 import utils
 
 # from config import Config
-
 _log_file_name = __file__.split("/")[-1].split(".")[0]
 logger.add(f"logs/tweetpipe_{_log_file_name}.log", rotation="1 day")
 
@@ -24,7 +32,7 @@ class Tweets:
         self.count = count
         self.user = self.get_user()
 
-        self._s3 = self._s3_client()
+        self.s3 = S3()
 
     def _auth(self):
         """Authenticate with Twitter and return a tweepy API inst."""
@@ -34,16 +42,9 @@ class Tweets:
 
         return tweepy.API(auth)
 
-    def _s3_client(self):
-        s3 = boto3.client(
-            "s3",
-            aws_access_key_id=settings.AWS_ACCESS_KEY,
-            aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-            region_name=settings.AWS_REGION_NAME,
-        )
-        return s3
-
     def get_user(self):
+        """Get the user referenced by the username"""
+        # TODO: Catch Unknown User Exception
         return self._api.get_user(self.username)
 
     @property
@@ -58,10 +59,13 @@ class Tweets:
         self.tweets = tweets
 
     def convert_tweets_to_json(self):
+        """Convert tweet data and metadata to json"""
         fetched_tweets = {
-            "fetched_at": utils.datetime_to_twitter_format(self.fetched_at),
-            "username": self.username,
-            "count": self.count,
+            "metadata": {
+                "fetched_at": utils.datetime_to_twitter_format(self.fetched_at),
+                "username": self.username,
+                "count": self.count,
+            },
             "tweets": [],
         }
         for tweet in self.tweets:
@@ -72,22 +76,26 @@ class Tweets:
         self.json_tweets = fetched_tweets
         return fetched_tweets
 
-    def upload_json_to_s3(self):
-        """Upload raw unformatted tweet data to S3 Bucket"""
-        key = f"{self.username}/{utils.datetime_to_string_format(self.fetched_at)}.json"
-        response = self._s3.put_object(Bucket=settings.AWS_BUCKET_NAME, Body=self.json_tweets, Key=key)
-        if response["ResponseMetadata"]["HTTPStatusCode"] == 200:
-            logger.info(response)
-            return None
-        else:
-            logger.warning(response)
-            return response
+    @property
+    def filename(self):
+        return f"{self.username}/{utils.datetime_to_string_format(self.fetched_at)}.json"
+
+    def upload(self):
+        """Convinience method wrapping storage.s3.upload"""
+        logger.debug(f"Uploading tweets to S3 with filename {self.filename}.")
+        self.s3.upload(self.filename, self.json_tweets)
 
 
 def get_tweet_data(username, count, upload_to_s3=True):
+    """
+    Entry function to extract count tweets from username
+
+    If upload_to_s3 is set to True, upload the raw file with appended
+    metadata to S3.
+    """
     tweets = Tweets(username=username, count=count)
     tweets.fetch()
     json_tweets = tweets.convert_tweets_to_json()
     if upload_to_s3:
-        response = tweets.upload_json_to_s3()
+        tweets.upload()
     return json_tweets
